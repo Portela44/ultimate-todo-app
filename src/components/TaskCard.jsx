7aabe38da3a6cc027d9a2f689999e68af078a41a
Guillem <guillem.mart7@gmail.com>
2022-08-17 20:37:23 +0200

    iteration 1 done

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -4,14 +4,17 @@ export default function TaskCard(props) {
     const {task: {name, image, useful_link, urgency, description, done}} = props;
     return (
         <div className="taskCard">
-            <h1 className="urgency">{urgency}</h1>
+            {/* Checkbox begins */}
+            {/* Checkbox ends */}
+            <input type="checkbox" value="done" />
             <img src={image} alt={name} />
             <div className="taskInfo">
-                <h3>{name}</h3>
+                <h2>{name}</h2>
                 <h5>{description}</h5>
                 <h5>{useful_link}</h5>
             </div>
-            <input type="checkbox" value="done" />
+            <h1 className="urgency">{urgency}</h1>
+            <button className="delete-btn">Delete</button>
         </div>
     )
 }
