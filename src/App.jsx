b8caef1fdb8f8458facb9dffe948770c10f5f5b1
Guillem <guillem.mart7@gmail.com>
2022-08-18 17:57:59 +0200

    added feature to send done list to a different list

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   const [filteredTasks, setFilteredTasks] = useState(tasks);
   const [showForm, setShowForm] = useState(false);
   const [sorted, setSorted] = useState(false);
+  const [doneTasks, setDoneTasks] = useState([]);
 
   const handleDelete = (name) => {
     const updatedTasks = pendingTasks.filter(task => task.name !== name);
@@ -34,18 +35,35 @@ function App() {
   const handleUrgency = () => {
     const copy = [...pendingTasks];
     if(sorted) {
-      setPendingTasks(copy.sort((a,b) => a.name - b.name));
+      setPendingTasks(copy.sort((a,b) => a.image - b.image));
     } else {
       setPendingTasks(copy.sort((a,b) => b.urgency - a.urgency));
     }
     setSorted(prev => !prev);
-    console.log(sorted);
   }
 
   //updates filteredTasks when new task is added, so new task is showed
   useEffect(() => {
     setFilteredTasks(pendingTasks)
   },[pendingTasks, sorted]);
+
+  const handleDoneTask = (doneTask, check) => {
+    const updatedTasks = pendingTasks.filter(task => task.name !== doneTask.name);
+    const updatedDoneTasks = doneTasks.filter(task => task.name !== doneTask.name);
+    if(!check) {
+      setPendingTasks(updatedTasks);
+      setFilteredTasks(updatedTasks);
+      const copy = [...doneTasks];
+      copy.unshift(doneTask);
+      setDoneTasks(copy);
+    } else {
+      setDoneTasks(updatedDoneTasks);
+      setFilteredTasks(updatedTasks);
+      const copy = [...pendingTasks];
+      copy.unshift(doneTask);
+      setPendingTasks(copy);
+    }
+  }
 
   return (
     <div className="App">
@@ -57,10 +75,15 @@ function App() {
       <input className="searchBar" placeholder="Buscar" type="text" onChange={(e) => handleSearch(e)}/>
       <button className="menu-btn" onClick={() => handleUrgency()}>{sorted ? "Back to normal":"Show urgent"}</button>
       {filteredTasks.map(task => {
-        return <TaskCard key={task.name} task={task} onDelete={handleDelete}/>
+        return <TaskCard key={task.name} task={task} onDelete={handleDelete} onDone={handleDoneTask}/>
       })}
       <br/>
       {filteredTasks.length===0 && <img className="noTasks" src="https://www.onlygfx.com/wp-content/uploads/2019/02/8-chalk-stroke-banner-3.png"/>}
+      {doneTasks.length!== 0 && <img className="dividingChalk" src="https://www.onlygfx.com/wp-content/uploads/2019/02/8-chalk-stroke-banner-1.png" alt="dividing chalk line" />}
+      {doneTasks.length!== 0 && <h1 className="listTitle">Done List</h1>}
+      {doneTasks.length!== 0 && doneTasks.map(task => {
+        return <TaskCard key={task.name} task={task} onDelete={handleDelete} onDone={handleDoneTask}/>
+      })}
     </div>
   );
 }
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function TaskCard(props) {
-    const {task: {name, image, useful_link, urgency, description, done}, onDelete} = props;
+    const {task: {name, image, useful_link, urgency, description, done}, onDelete, onDone} = props;
     return (
         <div className="taskCard" style={{borderImage: 'url("https://www.onlygfx.com/wp-content/uploads/2022/04/chalk-frame-2.png") 450'}}>
-            <input type="checkbox" value="done" />
+            <input type="checkbox" value="done" onChange={() => onDone(props.task, done)} />
             <img src={image} alt={name} />
             <div className="taskInfo">
                 <h2>{name}</h2>
